a8554d4e84e5740aaba58e01b4cbb900afbd9fc9
george petre <petregeorgee@gmail.com>
2023-05-29 10:42:55 +0300

    separate the encryption process from the decryption process

--- main.py
+++ main.py
@@ -25,13 +25,50 @@ def xor_n(gray_array, cheie_fluida_octeti, n):
     return enc_matrix1
 
 
+def write_array_to_file(array, filename):
+    with open(filename, 'w') as file:
+        for element in array:
+            file.write(str(element) + '\n')
+    print("Array successfully written to", filename)
+
+
 def build_fluid_key(x_quantificated_sequence, length):
     return [int("0b" + "".join([str(x) for x in x_quantificated_sequence[k:k + 8]]), 2) for k in
             range(length)]
 
 
-if __name__ == '__main__':
-    image_path = r"C:\Users\pegeorge\Downloads/lfff.jpeg"
+def encrypt(image_path):
+    image_to_one_dimension_array, image_shape = image_to_one_dimension(image_path)
+    x0 = 0.02
+    y0 = 0.01
+    z0 = 0.03
+    h = 0.001
+    n_steps = image_to_one_dimension_array.size - 1
+    x, y, z = discretization_lorenz_based_on_euler(x0, y0, z0, n_steps, h)
+    x_quantificated_sequence = array_quantification(x)
+    fluid_key = build_fluid_key(x_quantificated_sequence, len(image_to_one_dimension_array))
+
+    encryption_seq = xor_arrays(image_to_one_dimension_array, fluid_key)
+
+    encrypted_image = get_image_from_sequence(encryption_seq, image_shape)
+    encrypted_image.show()
+    encrypted_image.convert('L').save(r"C:\Users\pegeorge\Downloads\la\image.png")
+
+    # decrypted_seq = xor_arrays(encryption_seq, fluid_key)
+    # decrypted_image = get_image_from_sequence(decrypted_seq, image_shape)
+    # decrypted_image.show()
+
+
+def build_array_from_file(filename):
+    array = []
+    with open(filename, 'r') as file:
+        for line in file:
+            element = line.strip()
+            array.append(int(element))
+    return array
+
+
+def decrypt(image_path):
     image_to_one_dimension_array, image_shape = image_to_one_dimension(image_path)
 
     x0 = 0.02
@@ -40,18 +77,19 @@ if __name__ == '__main__':
     h = 0.001
 
     n_steps = image_to_one_dimension_array.size - 1
-
     x, y, z = discretization_lorenz_based_on_euler(x0, y0, z0, n_steps, h)
     x_quantificated_sequence = array_quantification(x)
-
     fluid_key = build_fluid_key(x_quantificated_sequence, len(image_to_one_dimension_array))
 
-    encryption_seq = xor_arrays(image_to_one_dimension_array, fluid_key)
-
-    encrypted_image = get_image_from_sequence(encryption_seq, image_shape)
-    encrypted_image.show()
-
-    decrypted_seq = xor_arrays(encryption_seq, fluid_key)
-
-    decrypted_image = get_image_from_sequence(decrypted_seq, image_shape)
+    dencryption_seq = xor_arrays(image_to_one_dimension_array, fluid_key)
+    decrypted_image = get_image_from_sequence(dencryption_seq, image_shape)
     decrypted_image.show()
+    decrypted_image.convert('L').save(r"C:\Users\pegeorge\Downloads\la\decrypted.jpeg")
+
+
+
+if __name__ == '__main__':
+    image_path = r"C:\Users\pegeorge\Downloads/lfff.jpeg"
+    encrypt(image_path)
+    image_path2 = r"C:\Users\pegeorge\Downloads\la\image.png"
+    decrypt(image_path2)
